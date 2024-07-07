49a3893f2abdf483d5177d49f730898871b7c121
Sven <34579211+sutidor@users.noreply.github.com>
2024-07-07 22:27:33 +0200

    Add link to Prompts page in the navbar - Part 2

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,19 +1,8 @@
-// deno-lint-ignore-file no-explicit-any
-import { Handlers, PageProps } from "$fresh/server.ts";
-import Layout from "../components/Layout.tsx";
-import { State } from "./_middleware.ts";
-
-export const handler: Handlers<any, State> = {
-  GET(_req, ctx) {
-    return ctx.render({...ctx.state});
-  }
+new Date(prompt.created_at).toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-
-export default function Home(props: PageProps) {
-  return (
-    <Layout isLoggedIn={props.data.token}>
-      <div class="mt-10 px-5 mx-auto flex max-w-screen-md flex-col justify-center">
-        {props.data.token ?
-          (
-            <div class="mx-auto text-center">
-              <a href="/prompts" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb
